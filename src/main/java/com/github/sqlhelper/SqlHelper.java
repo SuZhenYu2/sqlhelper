81d4edd55ec058d806e2aa5137cc49e16710cf3d
suzy2 <suzy2@lenovo.com>
2017-11-21 10:41:47 +0800

    完善sql处理

--- src/main/java/com/github/sqlhelper/SqlHelper.java
+++ src/main/java/com/github/sqlhelper/SqlHelper.java
@@ -25,135 +25,136 @@ import org.apache.log4j.Logger;
  * @author suzy2
  */
 @Intercepts({@Signature(type = StatementHandler.class, method = "query", args = {Statement.class, ResultHandler.class}),
-    @Signature(type = StatementHandler.class, method = "update", args = {Statement.class}),
-    @Signature(type = StatementHandler.class, method = "batch", args = { Statement.class })})
+	@Signature(type = StatementHandler.class, method = "update", args = {Statement.class}),
+	@Signature(type = StatementHandler.class, method = "batch", args = { Statement.class })})
 public class SqlHelper implements Interceptor {
 	private static Logger LOGGER=Logger.getLogger(SqlHelper.class);
-    @Override
-    public Object intercept(Invocation invocation) throws Throwable {
-        Object target = invocation.getTarget();
-        
-        long startTime = System.currentTimeMillis();
-        StatementHandler statementHandler = (StatementHandler)target;
-        try {
-            return invocation.proceed();
-        } finally {
-        	if(LOGGER.isDebugEnabled()){
-        		 try {
-					long endTime = System.currentTimeMillis();
-					 long sqlCost = endTime - startTime;
-					   // 格式化Sql语句，去除换行符，替换参数
-					 String sql = formatSql( statementHandler);
-					 sql =beautifySql(sql);
-					 LOGGER.debug("SQL：[" + sql + "]执行耗时[" + sqlCost + "ms]");
-				} catch (Exception e) {
-					LOGGER.error(e.getMessage(),e);
-				}
-        	}
-           
-        }
-    }
-
-    private String formatSql(StatementHandler statementHandler) {
-          
-//     	  Configuration configuration = session.getConfiguration();
-    	
-    	/*Configuration  configuration = null;
-           TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();*/
-    	BoundSql boundSql = statementHandler.getBoundSql();
-    	 Object params =boundSql.getParameterObject();
-    	 
-    	
-           List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
-           String sql = boundSql.getSql();
-           if (parameterMappings != null) {
-               for (int i = 0; i < parameterMappings.size(); i++) {
-                   ParameterMapping parameterMapping = parameterMappings.get(i);
-                   if (parameterMapping.getMode() != ParameterMode.OUT) {
-                       Object value;
-                       String propertyName = parameterMapping.getProperty();
-                       if (boundSql.hasAdditionalParameter(propertyName)) {
-                           value = boundSql.getAdditionalParameter(propertyName);
-                       } else if (params == null) {
-                           value = null;
-                       }/* else if (typeHandlerRegistry.hasTypeHandler(params.getClass())) {
-                           value = params;
-                       }*/else if( params instanceof String) {
-                    	   value =params;
-                       }else if( params instanceof Arrays) {
-                    	   value =params;
-                    	  } else {
-                           MetaObject metaObject = SystemMetaObject.forObject(params);
-                           value = metaObject.getValue(propertyName);
-                       }
-                       JdbcType jdbcType = parameterMapping.getJdbcType();
-                       if (value == null && jdbcType == null) jdbcType =JdbcType.NULL;
-                       sql = replaceParameter(sql, value, jdbcType, parameterMapping.getJavaType());
-                   }
-               }
-           }
-           return sql;
-	}
-    /**
-     * 根据类型替换参数
-     * 仅作为数字和字符串两种类型进行处理，需要特殊处理的可以继续完善这里
-     *
-     * @param sql
-     * @param value
-     * @param jdbcType
-     * @param javaType
-     * @return
-     */
-    private static String replaceParameter(String sql, Object value, JdbcType jdbcType, Class<?> javaType) {
-        String strValue = String.valueOf(value);
-        if (jdbcType != null) {
-            switch (jdbcType) {
-                //数字
-                case BIT:
-                case TINYINT:
-                case SMALLINT:
-                case INTEGER:
-                case BIGINT:
-                case FLOAT:
-                case REAL:
-                case DOUBLE:
-                case NUMERIC:
-                case DECIMAL:
-                    break;
-                //日期
-                case DATE:
-                case TIME:
-                case TIMESTAMP:
-                    //其他，包含字符串和其他特殊类型
-                default:
-                    strValue = "'" + strValue + "'";
-
-
-            }
-        } else if (Number.class.isAssignableFrom(javaType)) {
-            //不加单引号
-        } else {
-            strValue = "'" + strValue + "'";
-        }
-        return sql.replaceFirst("\\?", strValue);
-    }
 	@Override
-    public Object plugin(Object target) {
-        return Plugin.wrap(target, this);
-    }
+	public Object intercept(Invocation invocation) throws Throwable {
+		Object target = invocation.getTarget();
 
-    @Override
-    public void setProperties(Properties properties) {
-        
-    }
-   
-    /**
-     * 美化Sql
-     */
-    private String beautifySql(String sql) {
-        // sql = sql.replace("\n", "").replace("\t", "").replace("  ", " ").replace("( ", "(").replace(" )", ")").replace(" ,", ",");
-        sql = sql.replaceAll("[\\s\n ]+"," ");
-        return sql;
-    }
-    
+		long startTime = System.currentTimeMillis();
+		StatementHandler statementHandler = (StatementHandler)target;
+		try {
+			return invocation.proceed();
+		} finally {
+			if(LOGGER.isDebugEnabled()){
+				long endTime = System.currentTimeMillis();
+				long sqlCost = endTime - startTime;
+				// 格式化Sql语句，去除换行符，替换参数
+				String sql = formatSql( statementHandler);
+				sql =beautifySql(sql);
+				LOGGER.debug("SQL：[" + sql + "]执行耗时[" + sqlCost + "ms]");
+
+			}
+
+		}
+	}
+
+	private String formatSql(StatementHandler statementHandler) {
+
+		//     	  Configuration configuration = session.getConfiguration();
+
+		/*Configuration  configuration = null;
+           TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();*/
+		BoundSql boundSql = statementHandler.getBoundSql();
+		Object params =boundSql.getParameterObject();
+
+
+		List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+		String sql = boundSql.getSql();
+		try {
+			if (parameterMappings != null) {
+				for (int i = 0; i < parameterMappings.size(); i++) {
+					ParameterMapping parameterMapping = parameterMappings.get(i);
+					if (parameterMapping.getMode() != ParameterMode.OUT) {
+						Object value;
+						String propertyName = parameterMapping.getProperty();
+						if (boundSql.hasAdditionalParameter(propertyName)) {
+							value = boundSql.getAdditionalParameter(propertyName);
+						} else if (params == null) {
+							value = null;
+						}/* else if (typeHandlerRegistry.hasTypeHandler(params.getClass())) {
+			                   value = params;
+			               }*/else if( params instanceof String) {
+			            	   value =params;
+			               }else if( params instanceof Arrays) {
+			            	   value =params;
+			               } else {
+			            	   MetaObject metaObject = SystemMetaObject.forObject(params);
+			            	   value = metaObject.getValue(propertyName);
+			               }
+						JdbcType jdbcType = parameterMapping.getJdbcType();
+						if (value == null && jdbcType == null) jdbcType =JdbcType.NULL;
+						sql = replaceParameter(sql, value, jdbcType, parameterMapping.getJavaType());
+					}
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(),e);
+		}
+		return sql;
+	}
+	/**
+	 * 根据类型替换参数
+	 * 仅作为数字和字符串两种类型进行处理，需要特殊处理的可以继续完善这里
+	 *
+	 * @param sql
+	 * @param value
+	 * @param jdbcType
+	 * @param javaType
+	 * @return
+	 */
+	private static String replaceParameter(String sql, Object value, JdbcType jdbcType, Class<?> javaType) {
+		String strValue = String.valueOf(value);
+		if (jdbcType != null) {
+			switch (jdbcType) {
+			//数字
+			case BIT:
+			case TINYINT:
+			case SMALLINT:
+			case INTEGER:
+			case BIGINT:
+			case FLOAT:
+			case REAL:
+			case DOUBLE:
+			case NUMERIC:
+			case DECIMAL:
+				break;
+				//日期
+			case DATE:
+			case TIME:
+			case TIMESTAMP:
+				//其他，包含字符串和其他特殊类型
+			default:
+				strValue = "'" + strValue + "'";
+
+
+			}
+		} else if (Number.class.isAssignableFrom(javaType)) {
+			//不加单引号
+		} else {
+			strValue = "'" + strValue + "'";
+		}
+		return sql.replaceFirst("\\?", strValue);
+	}
+	@Override
+	public Object plugin(Object target) {
+		return Plugin.wrap(target, this);
+	}
+
+	@Override
+	public void setProperties(Properties properties) {
+
+	}
+
+	/**
+	 * 美化Sql
+	 */
+	private String beautifySql(String sql) {
+		// sql = sql.replace("\n", "").replace("\t", "").replace("  ", " ").replace("( ", "(").replace(" )", ")").replace(" ,", ",");
+		sql = sql.replaceAll("[\\s\n ]+"," ");
+		return sql;
+	}
+
 }
